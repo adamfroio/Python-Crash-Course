ff0632e29b627f88d3248e79ac83c6a8ab7a939a
Adam Froio <afroio@hotmail.com>
2020-11-18 10:21:20 -0800

    from my SurfaceBook

---
+++ Chapter 2/number.py
@@ -0,0 +1,4 @@
+
+universe_age = 14_000_000_000
+
+print(str(universe_age))
---
+++ Chapter 2/whitespace.py
@@ -0,0 +1,17 @@
+
+
+print("Python")
+
+print("\tPython")
+
+print("Languages:\nPython\nC\nJavaScript")
+
+favorite_language = "Python "
+
+print(f"***{favorite_language}***")
+
+favorite_language = favorite_language.rstrip()
+
+my_favorite_language = f"***{favorite_language}***"
+
+print(my_favorite_language)
